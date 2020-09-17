bb668403f2a230b8a50009106036d40e13ce0915
Andy Foster <andrewfosteris@gmail.com>
2020-09-17 13:38:31 +1200

    Initial commit

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var currentStoryNum = 0
+    let storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
 
     }
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if sender.titleLabel?.text == storyBrain.storyCollection[currentStoryNum].choice1 {
+            currentStoryNum = storyBrain.storyCollection[currentStoryNum].choice1Destination
+        } else {
+             currentStoryNum = storyBrain.storyCollection[currentStoryNum].choice2Destination
+        }
+        updateUI()
+    }
+    
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.storyCollection[currentStoryNum].title
+        choice1Button.setTitle(storyBrain.storyCollection[currentStoryNum].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.storyCollection[currentStoryNum].choice2, for: .normal)
+    }
 
 }
 
